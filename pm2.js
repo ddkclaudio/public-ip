84a5bb72a86b7740d00c6dcfe57b5146528292b4
Claudio Silva <ddk.claudio@gmail.com>
2019-03-16 11:20:55 -0300

    First

---
+++ pm2.js
@@ -0,0 +1,28 @@
+var pm2 = require('pm2');
+const services = []
+services.push('meu-ip.js')
+
+function options() {
+    return {
+        logDateFormat: "YYYY-MM-DD h:mm:ss A",
+        max_memory_restart: '1G',
+        watch: true,
+        autorestart: true
+    }
+}
+
+function errback(err, apps) {
+    pm2.disconnect();
+    if (err) throw err
+}
+
+pm2.connect(function (err) {
+    if (err) {
+        console.error("QERRO ::");
+        process.exit(2);
+    }
+
+    services.forEach(element => {
+        pm2.start(element, options(), errback)
+    });
+});
